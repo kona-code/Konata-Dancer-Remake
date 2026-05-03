1a61ef3465f6231f46bd7340cf0b380a0b39bbb9
ScriptCoolestIdkSomeOne <velikiydad@gmail.com>
2026-05-03 20:14:24 +0300

    clear

--- core/src/core.h
+++ core/src/core.h
@@ -1,11 +1,1 @@
-#pragma once
 
-class konacore {
-public:
-  konacore(const konacore &) = default;
-  konacore(konacore &&) = default;
-  konacore &operator=(const konacore &) = default;
-  konacore &operator=(konacore &&) = default;
-  constexpr static const char *project = "KonataDancer";
-  constexpr static short version[3] = {2, 0, 0};
-};
